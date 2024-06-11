63d626cfb1d45c36cbd10cbf8045b2cf9457fffb
Ayush Tiwari <ayush.1.tiwari@nokia.com>
2024-06-11 12:21:46 +0530

    Initial Commit

---
+++ ReelVibe/ReelVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelVibeApp.swift
+//  ReelVibe
+//
+//  Created by Ayush Tiwari on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ReelVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
